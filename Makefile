ffd925037d6c3b2b9e31ccedd682bf22600391c3
Andrew Chilton <andychilton@gmail.com>
2017-05-24 10:49:17 +1200

    Cache each feed and generated JSON file

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ test-index:
 test-ok: test-ok-chilts test-ok-scripting test-ok-freecodecamp
 
 test-ok-chilts-rss-minify:
-	curl --silent 'http://localhost:3000/convert?url=http://chilts.org/rss.xml' | json_pp
+	curl --silent 'http://localhost:3000/convert?url=http://chilts.org/rss.xml&minify=t' | json_pp
 	echo
 
 test-ok-chilts-rss-pretty:
--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 // core
 const http = require('http')
 const path = require('path')
+const crypto = require('crypto')
+const fs = require('fs')
 
 // npm
 const express = require('express')
@@ -10,9 +12,18 @@ const morgan = require('morgan')
 const request = require('request')
 const validUrl = require('valid-url')
 const FeedParser = require('feedparser')
+// const redis = require("redis")
 
 // local
 const booleanify = require('./lib/booleanify.js')
+const convert = require('./lib/convert.js')
+
+// --------------------------------------------------------------------------------------------------------------------
+// setup
+
+// let redisClient = redis.createClient()
+
+const cacheDir = '/var/lib/feed2json/cache'
 
 // --------------------------------------------------------------------------------------------------------------------
 // helpers
@@ -32,6 +43,17 @@ function getHeaderParams(header) {
     acc[kv[0].trim()] = ( kv[1] || '' ).trim()
     return acc
   }, {})
+}
+
+function sendJson(res, minify, data) {
+  if ( minify ) {
+    res.json(data)
+  }
+  else {
+    res.set({'Content-Type': 'application/json; charset=utf-8'})
+    res.status(200)
+    res.send(JSON.stringify(data, undefined, '  '))
+  }
 }
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -65,197 +87,76 @@ app.get('/convert', (req, res) => {
 
   console.log('validUrl=' + url)
 
-  // create the feedparser ready for when we get the request back
-  var feedparser = new FeedParser({
-    normalize       : true,
-    addmeta         : false,
-    feedurl         : url,
-    resume_saxerror : true,
-  })
+  // get the hash of this URL
+  let hashAcc = crypto.createHash('sha256')
+  hashAcc.update(url)
+  let hash = hashAcc.digest('hex').substr(0, 12)
+  console.log('hash=' + hash)
 
-  // start the request
-  const fetch = request(url, { timeout : 10000, pool : false })
-  fetch.setHeader('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246')
-  fetch.setHeader('accept', 'text/html,application/xhtml+xml')
-
-  // check for request errors
-  fetch.on('error', function (err) {
-    responseSent = true
-    return sendError(res, 500, "error when requesting the feed : " + err)
-  })
-
-  // Process the response when we get something back.
-  fetch.on('response', function(feed) {
-    console.log('request.response')
-    if ( feed.statusCode != 200 ) {
-      return this.emit('error', new Error('Bad status code'))
-    }
-
-    // See if this is a weird charset:
-    //
-    // * https://github.com/danmactough/node-feedparser/blob/master/examples/iconv.js
-    //
-    let headerParams = getHeaderParams(feed.headers['content-type'])
-    console.log('headerParams:', headerParams)
-    // feed = maybeTranslate(feed, headerParams.charset)
-
-    // finally, pipe into the feedparser
-    feed.pipe(feedparser)
-  })
-
-  // save data as we stumble upon it
-  let data = {}
-
-  // now listen to events from the feedparser
-  feedparser.on('error', function(err) {
-    console.log('feedparser error :', err)
-    responseSent = true
-    return sendError(res, 500, "error parsing feed : " + err)
-  })
-
-  feedparser.on('meta', function(meta) {
-    console.log('meta.link:', meta.link)
-    // console.log(meta)
-
-    // Going through fields in the same order as : https://jsonfeed.org/version/1
-
-    // version (required, string)
-    data.version = "https://jsonfeed.org/version/1"
-
-    // title (required, string)
-    data.title = meta.title
-
-    // home_page_url (optional)
-    if ( meta.link ) {
-      data.home_page_url = meta.link
-    }
-
-    // feed_url (optional, string) - is self-referencing, but we don't have anything here to reference since we're generating from either
-    // an RSS or Atom feed.
-
-    // description (optional, string)
-    if ( meta.description ) {
-      data.description = meta.description
-    }
-
-    // user_comment (optional, string) - nothing in RSS or Atom can be used here
-
-    // next_url (optional, string) - nothing in RSS or Atom can be used here
-
-    // icon (optional, string) - nothing in RSS or Atom can be used here
-
-    // favicon (optional, string) - Atom might have this
-    if ( meta.favicon ) {
-      data.favicon = meta.favicon
-    }
-
-    // author{name,url,avatar} (optional, must include one if exists)
-    if ( meta.author ) {
-      // even in Atom feeds with Author Name, Email and URI, feedparser only gives `meta.author`
-      data.author = {
-        name : meta.author,
-      }
-    }
-
-    // expired (optional, boolean) - nothing in RSS or Atom can be used here
-
-    // hubs (optional, array of objects) - ignoring for now
-
-    // items (array, required) - add this now for appending to later
-    data.items = []
-  })
-
-  feedparser.on('data', function(post) {
-    // console.log('feedparser.data')
-    console.log(' - post = ' + post.guid)
-
-    let item = {}
-
-    // Going through fields in the same order as : https://jsonfeed.org/version/1
-
-    // id (required, string) - use `guid`
-    if ( post.guid ) {
-      item.guid = post.guid
-    }
-    else {
-      // What should we do if there is no `guid` since `id` is required?
-    }
-
-    // url (optional, string) - the permalink if you like, may be the same as `id`
-    if ( post.link ) {
-      item.url = post.link
-    }
-    else {
-      // What should we do if there is no `link` since we really should have a `url` here?
-    }
-
-    // external_url (optional, string) - ignore since we're adding a `url` anyway
-
-    // title (optional, string)
-    if ( post.title ) {
-      item.title = post.title
-    }
-
-    // content_html/content_text (optional, string) - one must be present
-    if ( post.description ) {
-      item.content_html = post.description
-    }
-
-    // summary (optional, string)
-    if ( post.summary ) {
-      item.summary = post.summary
-    }
-
-    // image (optional, string)
-    if ( post.image ) {
-      if ( post.image.constructor === Object ) {
-        // skip for now
-      }
-      else {
-        item.image = post.image
-      }
-    }
-
-    // banner_image (optional, string) - ???
-
-    // date_published (optional, string)
-    if ( post.pubDate ) {
-      item.date_published = post.pubDate
-    }
-
-    // date_modified (optional, string) - ???
-
-    // author (optional, object)
-    if ( post.author ) {
-      item.author = {
-        name : post.author,
-      }
-    }
-
-    // tags (optional, string[])
-
-    // finally, push this `item` onto `data.items`
-    data.items.push(item)
-  })
-
-  // and finish the request
-  feedparser.on('end', function() {
-    console.log('feedparser.end')
-
-    // don't do anything if we have already errored out and sent a response
-    if ( responseSent ) {
+  // check to see if we can open the cached file and send it - nice and easy
+  let feedFilename = path.join(cacheDir, hash)
+  let jsonFilename = path.join(cacheDir, hash + '.json')
+  let jsonMinFilename = path.join(cacheDir, hash + '.min.json')
+  let filename = minify ? jsonMinFilename : jsonFilename
+  console.log('feed=' + feedFilename)
+  console.log('json=' + jsonFilename)
+  console.log('min =' + jsonMinFilename)
+  res.sendFile(filename, (err) => {
+    // if there was no error, then the JSON file exists and was sent
+    if ( !err ) {
+      console.log('Sent JSON file as response : ' + filename)
       return
     }
 
-    // alright to send the data
-    if ( minify ) {
-      res.json(data)
+    // file probably doesn't exist, but it could be a different error
+    if ( err.code !== 'ENOENT' ) {
+      console.error('Error calling res.sendFile:', err)
+      return sendError(res, 500, "Error opening cached file:" + err)
     }
-    else {
-      res.set({'Content-Type': 'application/json; charset=utf-8'})
-      res.status(200)
-      res.send(JSON.stringify(data, undefined, '  '))
-    }
+
+    // file doesn't exist yet (ENOENT) - that can be expected, so just carry on normally
+
+    // let's hit the URL to fetch the feed
+    let fetch = request(url, { timeout : 10000, pool : false }, (err, response, body) => {
+      if (err) {
+        console.error('Error calling request:', err)
+        return sendError(err, 500, "Error calling request:" + err)
+      }
+
+      // write this file out
+      fs.writeFile(feedFilename, body, (err) => {
+        if (err) {
+          console.error('Error calling fs.writeFile:', err)
+          return sendError(err, 500, "Error calling fs.writeFile:" + err)
+        }
+
+        // now process the body and get the data back
+        convert(feedFilename, url, body, (data) => {
+          // now, write the response to disk, but we can also write the body out to the user already
+          sendJson(res, minify, data)
+
+          // whilst the response is being sent, we can write this data to the two cached files
+          fs.writeFile(jsonFilename, JSON.stringify(data, undefined, '  '), (err) => {
+            if (err) {
+              return console.errro('Error writing JSON file ' + jsonFilename + ' : ' + err)
+            }
+            console.log('Written JSON file : ' + jsonFilename)
+          })
+
+          // and the minified version
+          fs.writeFile(jsonMinFilename, JSON.stringify(data), (err) => {
+            if (err) {
+              return console.errro('Error writing JSON file ' + jsonMinFilename + ' : ' + err)
+            }
+            console.log('Written JSON file : ' + jsonMinFilename)
+          })
+        })
+      })
+    })
+
+    // set some headers on the request
+    fetch.setHeader('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246')
+    fetch.setHeader('accept', 'text/html,application/xhtml+xml')
   })
 })
 
