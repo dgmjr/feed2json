fa542b018aa52fa00b1fc0de0173103940d9e344
Andrew Chilton <andychilton@gmail.com>
2017-06-05 16:19:20 +1200

    Remove old feeds every hour

--- server.js
+++ server.js
@@ -2,9 +2,28 @@
 
 // core
 const http = require('http')
+const childProcess = require('child_process')
 
 // local
 const app = require('./lib/app.js')
+const constant = require('./lib/constant.js')
+const log = require('./lib/log.js')
+
+// --------------------------------------------------------------------------------------------------------------------
+// setup
+
+setInterval(() => {
+  log('Cleaning up')
+
+  childProcess.execFile('find', [ constant.cacheDir, '-mmin', '+60', '-delete' ], (err, stdout, stderr) => {
+    if (err) {
+      console.warn('Error removing old cache files: ' + err)
+      return
+    }
+    log('Removed old files')
+  })
+
+}, constant.cleanUpInterval)
 
 // --------------------------------------------------------------------------------------------------------------------
 // server
@@ -12,7 +31,7 @@ const app = require('./lib/app.js')
 const port = process.env.PORT || 3000
 const server = http.createServer(app)
 server.listen(port, () => {
-  console.log('Listening on port %s', port)
+  log('Listening on port ' + port)
 })
 
 // --------------------------------------------------------------------------------------------------------------------
