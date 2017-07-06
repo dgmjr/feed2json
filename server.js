153e3e7e69696ecc91575efac48fba489dd12971
Andrew Chilton <andychilton@gmail.com>
2017-07-07 11:02:23 +1200

    Delete the cached files and fix deletion of the cache directory

--- server.js
+++ server.js
@@ -2,28 +2,10 @@
 
 // core
 const http = require('http')
-const childProcess = require('child_process')
 
 // local
 const app = require('./lib/app.js')
-const constant = require('./lib/constant.js')
 const log = require('./lib/log.js')
-
-// --------------------------------------------------------------------------------------------------------------------
-// setup
-
-setInterval(() => {
-  log('Cleaning up')
-
-  childProcess.execFile('find', [ constant.cacheDir, '-mmin', '+60', '-delete' ], (err, stdout, stderr) => {
-    if (err) {
-      console.warn('Error removing old cache files: ' + err)
-      return
-    }
-    log('Removed old files')
-  })
-
-}, constant.cleanUpInterval)
 
 // --------------------------------------------------------------------------------------------------------------------
 // server
